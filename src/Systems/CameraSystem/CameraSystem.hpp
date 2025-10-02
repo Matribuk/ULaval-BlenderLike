49eb8af2b5a4d9caa3830fdfecee54ab77e9ff8e
Maskalito <clement.barrier@epitech.eu>
2025-10-02 18:34:22 -0400

    [ADD] camera related method

--- src/Systems/CameraSystem/CameraSystem.hpp
+++ src/Systems/CameraSystem/CameraSystem.hpp
@@ -6,17 +6,35 @@
 #include "Components/Transform.hpp"
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <vector>
+#include <iostream>
 
 class CameraSystem {
-public:
-    CameraSystem(ComponentRegistry& registry, EntityManager& entityMgr);
-    ~CameraSystem() = default;
+    public:
+        CameraSystem(ComponentRegistry& registry, EntityManager& entityMgr);
 
-    void update(int viewportWidth, int viewportHeight);
+        void update();
 
-    EntityManager& getEntityManager();
-    ComponentRegistry& getRegistry();
-private:
-    ComponentRegistry& _registry;
-    EntityManager& _entityManager;
+        void panKeyboard(float horizontal, float vertical);
+        void orbitKeyboard(float horizontal, float vertical);
+        void zoom(float amount);
+        void focusTarget();
+
+        void addCamera(EntityID cameraID);
+        void setActiveCamera(EntityID id);
+        void switchCamera();
+
+        Camera* getActiveCamera() const;
+        EntityID getActiveCameraId() const;
+
+    private:
+        ComponentRegistry& _registry;
+        EntityManager& _entityManager;
+
+        EntityID _activeCamera{INVALID_ENTITY};
+        std::vector<EntityID> _cameraEntities;
+        int _activeCameraIndex{0};
+
+        glm::vec3 _getCameraForward(EntityID id) const;
+        glm::vec3 _getCameraRight(EntityID id) const;
 };
